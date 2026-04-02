21ce45290dedf9fe93d192276c6b8c568d3fcc7c
dntsstudio-ai <dnts.studio@gmail.com>
2026-04-02 17:08:40 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,9 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { firebaseConfig } from "./config.js";
 
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
