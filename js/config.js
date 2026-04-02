79124bc1eb22a8bacca84e3bc4b7f475017d4588
dntsstudio-ai <dnts.studio@gmail.com>
2026-04-02 17:08:11 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,15 @@
+// js/config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyD34TTNdG1V4SZmuQ1ULVdo3iBB3q-7pC8",
+    authDomain: "my-dub-site.firebaseapp.com",
+    projectId: "my-dub-site",
+    storageBucket: "my-dub-site.firebasestorage.app",
+    messagingSenderId: "302896869866",
+    appId: "1:302896869866:web:a003a8f24d682cb3ac0293"
+};
 
+export const emailConfig = {
+    serviceId: 'service_sws',
+    templateSuggestId: 'template_suggest',
+    publicKey: 'FExFPIAtSKcFcS2yy'
+};
